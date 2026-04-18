eac1e995d9f4997f52bd4bb6ee254756c65af935
santo1616 <santo1616@gmail.com>
2026-04-18 09:04:40 -0500

    Parte 2 completa

---
+++ cazando.js
@@ -0,0 +1,36 @@
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+
+const ALTO_GATO = 80;
+const ANCHO_GATO = 80;
+const ALTO_COMIDA = 40;
+const ANCHO_COMIDA = 40;
+
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+function iniciarJuego() {
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+
+    graficarGato();
+    graficarComida();
+}
+
+function graficarGato() {
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "green");
+}
+
+function graficarComida() {
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "yellow");
+}
+
+function graficarRectangulo(x, y, ancho, alto, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+}
